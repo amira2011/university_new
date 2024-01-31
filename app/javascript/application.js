c3675d00b27662154773742ad065913f3204c55a
amira2011 <shaikh.abidali@gmail.com>
2024-01-31 22:13:52 +0530

    Font Awesome

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,16 +1,17 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+ 
 import "@hotwired/turbo-rails"
 import "controllers"
-
 import "popper"
-
-import "bootstrap"
-
+import "@fortawesome/fontawesome-free";
+//= require jquery
+//= require bootstrap
 import "jquery"
 import "jquery_ujs"
 import "./jquery_ui"
-import "@fortawesome/fontawesome-free";
-
-
+import "bootstrap"
+console.log($); // ok
+console.log("ok"); // ok
  
- $(".notice").fadeOut(4000)
+$(".notice").fadeOut(4000)
+ 
--- config/importmap.rb
+++ config/importmap.rb
@@ -1,16 +1,18 @@
 # Pin npm packages by running ./bin/importmap
 
+
+
 pin "application"
 pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
-
 pin "popper", to: 'popper.js', preload: true
-
-pin "bootstrap", to: 'bootstrap.min.js', preload: true
-
 pin "jquery", to: "jquery.min.js", preload: true
 pin "jquery_ujs", to: "jquery_ujs.js", preload: true
 pin "jquery-ui", to: "jquery-ui.min.js", preload: true
+
+pin "bootstrap", to: 'bootstrap.min.js', preload: true
+ 
 pin "@fortawesome/fontawesome-free", to: "https://ga.jspm.io/npm:@fortawesome/fontawesome-free@6.1.1/js/all.js"
+ 
