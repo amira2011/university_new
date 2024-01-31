0868aed7b479c16c00eba8afa83fb10d2dc59edd
amira2011 <shaikh.abidali@gmail.com>
2024-01-31 22:02:29 +0530

    ok

---
+++ app/javascript/application.js
@@ -0,0 +1,16 @@
+// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+import "@hotwired/turbo-rails"
+import "controllers"
+
+import "popper"
+
+import "bootstrap"
+
+import "jquery"
+import "jquery_ujs"
+import "./jquery_ui"
+import "@fortawesome/fontawesome-free";
+
+
+ 
+ $(".notice").fadeOut(4000)
