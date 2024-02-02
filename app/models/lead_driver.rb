6e45d989a9ebecd87315b613643ea2cdd1f3c33a
amira2011 <shaikh.abidali@gmail.com>
2024-02-02 22:40:22 +0530

    Mapping

--- app/models/lead_driver.rb
+++ app/models/lead_driver.rb
@@ -2,4 +2,22 @@ class LeadDriver < ApplicationRecord
   belongs_to :lead
   has_many :lead_vehicles
   has_many :lead_violations
+
+
+  MARITAL_STATUS_MAPPING = {
+  'Happily Married' => 'Married',
+  'Not Married' => 'Single',
+  'breakup' => 'Separated',
+  'dissolution' => 'Divorced'
+}
+
+  VALID_MARITAL_STATUS = ['Single', 'Married', 'Divorced', 'Separated', 'Widowed', 'Domestic Partner', 'Unknown']
+  validates :marital_status, inclusion: { in: VALID_MARITAL_STATUS, message: "Invalid Marital Status" }
+
+  def self.transform_marital_status_for_api(value)
+    mapped_value = MARITAL_STATUS_MAPPING[value] || value
+    VALID_MARITAL_STATUS.include?(mapped_value) ? mapped_value : 'Unknown'
+  end
+ 
+
 end
